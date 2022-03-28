17fe9daed61c15974a0b282d825421d082f0d8a8
Eunice Dhivya <divi.seed@gmail.com>
2022-03-28 17:08:45 +0530

    update local mongodb connections

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
 
 dotenv.config();
-console.log(process.env.PORT);
+// console.log(process.env.MONGO_URL);
 
 const app = express();
 
@@ -12,9 +13,176 @@ app.use(express.json());
 
 // Port for localhost
 const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+
+const DB_NAME = "assignmentor";
+
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("MongoDB connected...");
+  return client;
+}
+
+const client = await createConnection();
 
 app.get("/", function (request, response) {
   response.send("Assign Mentor API");
+});
+
+app.get("/students", async function (request, response) {
+  const result = await client
+    .db(DB_NAME) //DB Name
+    .collection("students") //Collection Name
+    .find({})
+    .toArray();
+
+  //   console.log("result", result);
+  response.send(result);
+});
+app.get("/unassignedstudents", async function (request, response) {
+  const result = await client
+    .db(DB_NAME) //DB Name
+    .collection("students") //Collection Name
+    .find({ mentor_id: "" })
+    .toArray();
+
+  //   console.log("result", result);
+  // response.send(result);
+  result
+    ? response.send(result)
+    : response.status(404).send({
+        msg: "No Students Found",
+      });
+});
+
+app.post("/students", async function (request, response) {
+  const newStudent = request.body;
+  const result = await client
+    .db(DB_NAME) //DB Name
+    .collection("students") //Collection Name
+    .insertMany(newStudent);
+  //   console.log("result", result);
+  response.send(result);
+});
+
+app.get("/students/:id", async function (request, response) {
+  const { id } = request.params;
+
+  console.log(id);
+  const student = await client
+    .db(DB_NAME) //DB Name
+    .collection("students") //Collection Name
+    .findOne({ _id: ObjectId(id) });
+
+  console.log(student);
+
+  student
+    ? response.send(student)
+    : response.status(404).send({
+        msg: "No Student Found",
+      });
+});
+
+app.put("/students/:id", async function (request, response) {
+  // const { id } = request.params;
+  const updateData = request.body;
+  const result = await client
+    .db(DB_NAME) //DB Name
+    .collection("students")
+    .updateOne({ _id: ObjectId(request.params.id) }, { $set: updateData });
+
+  console.log("result", result);
+  response.send(result);
+});
+
+app.delete("/students/:id", async function (request, response) {
+  const { id } = request.params;
+
+  console.log(id, typeof id);
+
+  const result = await client
+    .db(DB_NAME) //DB Name
+    .collection("students") //Collection Name
+    .deleteOne({ _id: ObjectId(id) });
+
+  console.log(result);
+
+  result
+    ? response.send(result)
+    : response.status(404).send({
+        msg: "No result Found",
+      });
+});
+
+app.get("/mentors", async function (request, response) {
+  const result = await client
+    .db(DB_NAME) //DB Name
+    .collection("mentors") //Collection Name
+    .find({})
+    .toArray();
+
+  //   console.log("result", result);
+  response.send(result);
+});
+
+app.post("/mentors", async function (request, response) {
+  const newStudent = request.body;
+  const result = await client
+    .db(DB_NAME) //DB Name
+    .collection("mentors") //Collection Name
+    .insertMany(newStudent);
+  //   console.log("result", result);
+  response.send(result);
+});
+
+app.put("/mentors/:id", async function (request, response) {
+  // const { id } = request.params;
+  const updateData = request.body;
+  const result = await client
+    .db(DB_NAME) //DB Name
+    .collection("mentors")
+    .updateOne({ _id: ObjectId(request.params.id) }, { $set: updateData });
+
+  console.log("result", result);
+  response.send(result);
+});
+
+app.delete("/mentors/:id", async function (request, response) {
+  const { id } = request.params;
+
+  console.log(id, typeof id);
+
+  const result = await client
+    .db(DB_NAME) //DB Name
+    .collection("mentors") //Collection Name
+    .deleteOne({ _id: ObjectId(id) });
+
+  console.log(result);
+
+  result
+    ? response.send(result)
+    : response.status(404).send({
+        msg: "No result Found",
+      });
+});
+
+app.get("/mentors/:id", async function (request, response) {
+  const { id } = request.params;
+
+  console.log(id);
+  const mentor = await client
+    .db(DB_NAME) //DB Name
+    .collection("mentors") //Collection Name
+    .findOne({ _id: ObjectId(id) });
+
+  console.log(mentor);
+
+  mentor
+    ? response.send(mentor)
+    : response.status(404).send({
+        msg: "No mentor Found",
+      });
 });
 
 app.listen(PORT, () => console.log("Server is started in " + PORT));
